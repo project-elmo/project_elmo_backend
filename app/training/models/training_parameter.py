8a3b89b6c924f807b9453d2adefa44ce21fff137
harimjung <harim.jung0727@gmail.com>
2023-08-25 21:40:20 +0900

    [ADD] Add extra columns to training_paramter

--- app/training/models/training_parameter.py
+++ app/training/models/training_parameter.py
@@ -1,5 +1,7 @@
 from sqlalchemy import (
+    Boolean,
     Column,
+    Float,
     Integer,
     String,
     ForeignKey,
@@ -21,9 +23,17 @@ class TrainingParameter(Base):
         primary_key=True,
         comment="AUTO_INCREMENT",
     )
-    parameters = Column(
-        String(1000),
-        nullable=False,
-        comment='json 형식의 긴 텍스트로 저장. 예: {"batch_size": 32, "learning_rate": 1e2222, ...}',
-    )
+    model_name = Column(String(255), nullable=False)
+    epochs = Column(Integer, nullable=False)
+    save_strategy = Column(String(50), nullable=False)
+    logging_strategy = Column(String(50), nullable=False)
+    evaluation_strategy = Column(String(50), nullable=False)
+    learning_rate = Column(Float, nullable=False)
+    weight_decay = Column(Float, nullable=False)
+    batch_size = Column(Integer, nullable=False)
+    eval_steps = Column(Integer, nullable=False)
+    save_steps = Column(Integer, nullable=False)
+    save_total_limits = Column(Integer)  # 'unlimited' is represented as -1
+    run_on_gpu = Column(Boolean, nullable=False)
+    load_best_at_the_end = Column(Boolean, nullable=False)
     dataset = Column(String(255), nullable=False, comment="훈련에 쓰인 dataset file path")
