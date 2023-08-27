1ee57721a0a09ad2b8483b0e986b844f8eb2caa9
harimjung <harim.jung0727@gmail.com>
2023-08-27 22:58:30 +0900

    [MOD] Update gunicorn config

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 20s
-alembic upgrade head && gunicorn --bind 0.0.0.0:8000 --workers 8 --threads 8 --timeout 3000 -k uvicorn.workers.UvicornWorker app.server:app
+alembic upgrade head && gunicorn --bind 0.0.0.0:8000 --workers 1 --threads 8 --timeout 3000 -k uvicorn.workers.UvicornWorker app.server:app
