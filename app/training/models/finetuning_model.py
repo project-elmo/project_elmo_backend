589e330d8e6c98aa34f537ee669e10fb523c7bf7
harimjung <harim.jung0727@gmail.com>
2023-08-28 16:16:23 +0900

    [MOD] Add default value for fm_description

--- app/training/models/finetuning_model.py
+++ app/training/models/finetuning_model.py
@@ -13,7 +13,9 @@ class FinetuningModel(Base):
     pm_no = Column(Integer, ForeignKey("pretrained_model.pm_no"), nullable=False)
     fm_name = Column(String(50), nullable=False, comment="파인튜닝 모델의 이름")
     fm_description = Column(
-        String(255), comment="해당 파인튜닝 모델에 대한 설명(*디자인에 반영안됨, 우선순위 낮음)"
+        String(255),
+        default="no description",
+        comment="해당 파인튜닝 모델에 대한 설명(*디자인에 반영안됨, 우선순위 낮음)",
     )
     task = Column(
         Integer, nullable=False, comment="모델의 목적:: 0 QA 1 Classification 2 Generate"
