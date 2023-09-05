d361e2f78190c7bcb3504f85f98085c044141489
jharinn <alfeka07@gmail.com>
2023-09-05 08:08:02 +0000

    [FIX] Fix worker timeout

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 dockerize -wait tcp://db:3306 -timeout 20s
-alembic upgrade head && gunicorn --bind 0.0.0.0:80 --workers 4 --threads 8 --timeout 3000 -k uvicorn.workers.UvicornWorker app.server:app --reload
+alembic upgrade head && gunicorn --bind 0.0.0.0:80 --workers 4 --threads 8 --timeout 10000 -k uvicorn.workers.UvicornWorker app.server:app --reload
